76f0070efea66c058c3c921a4149b8ee8c4f2949
MelanieOC <melanie.ocharan@gmail.com>
2017-08-28 07:12:46 -0500

    before

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,33 +20,43 @@ for (var i = 1; i < 10; i++) {
 
 function creaModal(e){
   var padre=e.parentNode;
-  var hermano=padre.nextSibling;
   var div=document.createElement('div');
   var content=document.createElement('div');
   var img=document.createElement('img');
   var close=document.createElement('button');
   var next=document.createElement('button');
+  var before=document.createElement('button');
   close.onclick=function() {
     padre.removeChild(padre.lastChild);
   }
 
   next.onclick=function() {
     padre.removeChild(padre.lastChild);
-    e=hermano.firstChild;
+    e=padre.nextSibling.firstChild;
+    creaModal(e);
+  }
+  before.onclick=function() {
+    padre.removeChild(padre.lastChild);
+    e=padre.previousSibling.firstChild;
     creaModal(e);
   }
   img.src=e.src;
   close.innerHTML='X';
   next.innerHTML='>';
+  before.innerHTML='<';
   div.setAttribute("class",'modal');
   content.setAttribute("class",'content');
   close.setAttribute("class",'close');
   next.setAttribute("class",'next');
+  before.setAttribute("class",'before');
 
   content.appendChild(img);
   content.appendChild(close);
-  if(hermano){
+  if(padre.nextSibling){
     content.appendChild(next);
+  }
+  if(padre.previousSibling){
+    content.appendChild(before);
   }
   div.appendChild(content);
   padre.appendChild(div);
